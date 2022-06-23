305fcee476b3c82eb3ed0284d5813586cec18e49
DESKTOP-PFMOHCA\jonny <jonny1996_ty@yahoo.co.jp>
2022-06-23 20:02:31 +0900

    routeを変更し忘れてた

--- src/functions/websocket/onDisconnect/index.ts
+++ src/functions/websocket/onDisconnect/index.ts
@@ -4,7 +4,7 @@ export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
     {
-      websocket: { route: "$connect" },
+      websocket: { route: "$disconnect" },
     },
   ],
 };
